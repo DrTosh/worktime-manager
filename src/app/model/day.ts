da31ceb1fc98a0bf552cde4432c881639c6024a5
DrTosh <DoktorTosh@gmail.com>
2020-02-10 02:16:52 +0100

    edit times to new Time Class

--- src/app/model/day.ts
+++ src/app/model/day.ts
@@ -1,20 +1,22 @@
+import { Time } from './time';
+
 export class Day {
     active: boolean;
     date: Date;
-    start: Date;
-    end: Date;
-    break: Date;
-    actualworktime: Date;
-    targetworktime: Date;
+    start: Time;
+    end: Time;
+    break: Time;
+    actualworktime: Time;
+    targetworktime: Time;
 
     constructor(date: Date) {
         this.active = true;
         this.date = new Date(date.setHours(0, 0, 0, 0));
-        this.start = new Date(date.setHours(0, 0, 0, 0));
-        this.end = new Date(date.setHours(0, 0, 0, 0));
-        this.break = new Date(date.setHours(0, 0, 0, 0));
-        this.actualworktime = new Date(date.setHours(0, 0, 0, 0));
-        this.targetworktime = new Date(date.setHours(0, 0, 0, 0));
+        this.start = new Time(0, 0);
+        this.end = new Time(0, 0);
+        this.break = new Time(0, 0);
+        this.actualworktime = new Time(0, 0);
+        this.targetworktime = new Time(0, 0);
     }
 
     // constructor (day: Day) {
