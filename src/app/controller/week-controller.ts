bf67a00556f03b90f327f4f56c88238ee35b02da
DrTosh <DoktorTosh@gmail.com>
2020-02-10 19:34:39 +0100

    Week Calc

--- src/app/controller/week-controller.ts
+++ src/app/controller/week-controller.ts
@@ -1,3 +1,13 @@
-export class WeekController {
+import { Week } from '../model/week';
+import { Time } from '../model/time';
 
+export class WeekController {
+    calcTimeSum(week: Week) {
+        week.actualworktime = new Time(0, 0);
+        week.targetworktime = new Time(0, 0);
+        week.days.forEach(day => {
+            week.actualworktime.add(day.actualworktime);
+            week.targetworktime.add(day.targetworktime);
+        });
+    }
 }
